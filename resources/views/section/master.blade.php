f1fc1528cce8eb1bbc572f883a7e1cb131f9ae6c
Than Htut Zaw <zgyi025@gmail.com>
2021-11-12 17:45:29 +0630

    link nav blade

--- resources/views/section/master.blade.php
+++ resources/views/section/master.blade.php
@@ -26,19 +26,12 @@
 </head>
 
 <body>
-        <nav class=" navbar navbar-primary bg-primary text-light ">
-            <div class="container-fluid">
-                <a class="text-light navbar-brand">Logo</a>
-                <form class="d-flex input-group w-auto mx-auto">
-                    <ul>
-                        <li><a href="{{url('/')}}">Home</a></li>
-                        <li><a href="{{url('movie')}}">Movie</a></li>
-                        <li><a href="{{url('admin')}}">Admin</a></li>
-                        <li><a href="{{url('login')}}">Login</a></li>
-                    </ul>
-                </form>
-            </div>
-        </nav>
+    <div class="container">
+        <div class="row">
+            @include('section.nav')
+        </div>
+    </div>
+
 
 
     @yield('content')
---
+++ resources/views/section/nav.blade.php
@@ -0,0 +1,13 @@
+<nav class=" navbar navbar-primary bg-primary text-light ">
+                <div class="container-fluid">
+                    <a class="text-light navbar-brand">Logo</a>
+                    <form class="d-flex input-group w-auto mx-auto">
+                        <ul>
+                            <li><a href="{{url('/')}}">Home</a></li>
+                            <li><a href="{{url('movie')}}">Movie</a></li>
+                            <li><a href="{{url('admin')}}">Admin</a></li>
+                            <li><a href="{{url('login')}}">Login</a></li>
+                        </ul>
+                    </form>
+                </div>
+</nav>
