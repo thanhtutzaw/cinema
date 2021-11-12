7f289c593e728265aac544f8772740d3753218d2
Than Htut Zaw <zgyi025@gmail.com>
2021-11-12 09:52:31 +0630

    login form bootstrap

--- resources/views/child/admin.blade.php
+++ resources/views/child/admin.blade.php
@@ -2,12 +2,54 @@
 
 @section('subtitle','Admin-')
 
-
+@section('style')
+.container{
+    padding:2rem 0 2rem 0;
+}
+form{
+    margin-top:2rem;
+}
+@endsection
 
 
 @section('content')
+<div class="container text-center">
 
-<button class="btn btn-danger">Admin page</button>
+    <div class="row">
+        <div class="col"></div>
+        <div class="col-5 col-lg-3 col-sm-4"><h1>Admin Page</h1></div>
+        <div class="col"></div>
+    </div>
+
+    <div class="row">
+        <div class="col"></div>
+
+        <div class="col-5 col-sm-5 col-md-5 col-lg-3 ">
+            <form>
+                <div class="row">
+                    <div class="col-9"><input type="text" class="mb-4 form-control" placeholder="Enter Phone"></div>
+                    <div class="col-3"><button class=" mb-4 form-control-sm btn-outline-dark">Verify</button></div>
+                </div>
+
+
+
+                <input type="text" class="mb-4 form-control" placeholder="Verify Code">
+                <button class="form-control btn-success">Login</button>
+            </form>
+        </div>
+
+        <div class="col"></div>
+
+
+    </div>
+
+</div>
+
+
+
+
+
+
 
 
 @endsection
--- resources/views/child/login.blade.php
+++ resources/views/child/login.blade.php
@@ -1,8 +1,47 @@
 @extends('section.master')
 @section('subtitle','Login-')
-@section('content')
 
-<button class="btn btn-secondary">Login</button>
-fdj
+@section('style')
+.container{
+    padding:2rem 0 2rem 0;
+}
+form{
+    margin-top:2rem;
+}
+@endsection
+
+@section('content')
+<div class="container text-center">
+
+    <div class="row">
+        <div class="col"></div>
+        <div class="col-5 col-lg-3 col-sm-4"><h1>Login</h1></div>
+        <div class="col"></div>
+    </div>
+
+    <div class="row">
+        <div class="col"></div>
+
+        <div class="col-5 col-sm-5 col-md-5 col-lg-3 ">
+            <form>
+                <div class="row">
+                    <div class="col-9"><input type="text" class="mb-4 form-control" placeholder="Enter Phone"></div>
+                    <div class="col-3"><button class=" mb-4 form-control-sm btn-outline-dark">Verify</button></div>
+                </div>
+
+
+
+                <input type="text" class="mb-4 form-control" placeholder="Verify Code">
+                <button class="form-control btn-success">Login</button>
+            </form>
+        </div>
+
+        <div class="col"></div>
+
+
+    </div>
+
+</div>
+
 
 @endsection
